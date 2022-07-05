01b49986639bea979b9c131f880fbf3c848524ab
been232 <kean503@naver.com>
2022-07-05 22:47:54 +0900

    마진, float clear 변경

--- front/src/UI/organisms/Header/HeaderOrganisms.js
+++ front/src/UI/organisms/Header/HeaderOrganisms.js
@@ -1,27 +1,41 @@
 import Box from '@mui/material/Box';
-import HeaderMolecules from "../../molecules/Header/HeaderMolecules";
+import HeaderMolecules from '../../molecules/Header/HeaderMolecules';
 
 function HeaderOrganisms(props) {
-	return (
-		<>
-		<Box
-			sx={{ 
-				borderBottom : 2,
-				height: 70
-			}}
-		>
-			<Box
-				sx={{
-					marginLeft: 5,
-					float: 'left'
-				}}
-			>
-				<img src={process.env.PUBLIC_URL + '/Logo.png'} alt="Logo" width="15%" />
-			</Box>
-			<HeaderMolecules />
-		</Box>
-		</>
-	);
+  return (
+    <>
+      <Box
+        sx={{
+          borderBottom: 2,
+          height: 70,
+          marginLeft: 20,
+          marginRight: 20,
+          '&::after': {
+            content: 'none',
+            display: 'none',
+            clear: 'both',
+          },
+        }}
+      >
+        <Box
+          sx={{
+            marginLeft: 5,
+            float: 'left',
+          }}
+        >
+          <img
+            src={process.env.PUBLIC_URL + '/Logo.png'}
+            alt="Logo"
+            width="17%"
+            style={{
+              marginLeft: -40,
+            }}
+          />
+        </Box>
+        <HeaderMolecules />
+      </Box>
+    </>
+  );
 }
 
 export default HeaderOrganisms;
