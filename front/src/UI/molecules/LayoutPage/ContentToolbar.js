bfdde534d81d5a35260fca497c402d805d5373b7
ilovedeo <55009603+ilovedeo@users.noreply.github.com>
2022-07-11 16:05:38 +0900

    임시

---
+++ front/src/UI/molecules/LayoutPage/ContentToolbar.js
@@ -0,0 +1,19 @@
+import { Box } from '@mui/system';
+
+export default function ContentToolbar() {
+  return (
+    <Box sx={{ marginTop: 5 }}>
+      <div
+        style={{
+          border: '1px solid black',
+          width: '99%',
+          height: 30,
+          marginLeft: 5,
+          marginBottom: 10,
+        }}
+      >
+        gg
+      </div>
+    </Box>
+  );
+}
--- front/src/UI/pages/LayoutPage/LayoutPage.js
+++ front/src/UI/pages/LayoutPage/LayoutPage.js
@@ -1,11 +1,13 @@
 import { Box } from '@mui/material';
 import React, { useState, useEffect } from 'react';
 import Content from '../../organisms/LayoutPage/Content';
+import Title from '../../organisms/LayoutPage/Title';
 
 function LayoutPage() {
   return (
     <>
       <Box sx={{ marginLeft: 20, marginRight: 20 }}>
+        <Title />
         <Content />
       </Box>
     </>
