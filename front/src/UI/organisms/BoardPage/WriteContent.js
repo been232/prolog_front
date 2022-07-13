b17332d9f448931a8f30c42d0308dc4529dd7cdf
ilovedeo <55009603+ilovedeo@users.noreply.github.com>
2022-07-13 17:05:09 +0900

    content 여러개 제작

--- front/src/UI/organisms/BoardPage/WriteContent.js
+++ front/src/UI/organisms/BoardPage/WriteContent.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
@@ -7,6 +7,7 @@ import Box from '@mui/material/Box';
 import Dropdown from '../../molecules/BoardPage/FontDropdown';
 import ArticleOutlinedIcon from '@mui/icons-material/ArticleOutlined';
 import TextArea from '../../molecules/BoardPage/TextArea';
+import { height } from '@mui/system';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -30,10 +31,84 @@ TabPanel.propTypes = {
 
 export default function WriteContent() {
   const [value, setValue] = useState('content');
+  const [count, setCount] = useState(0);
+  const [arr, setArr] = useState([]);
+  const [maxHeight, setmaxHeight] = useState(0);
+  const [Height, setHeight] = useState(0);
+
+  useEffect(() => {{max(arr)}}, [arr, maxHeight, Height, count]);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
+  const handleArrChange = (newValue) => {
+    const content = {
+      id: newValue.id,
+      context: newValue.content,
+      width: newValue.width,
+      height: newValue.height,
+      x: newValue.x,
+      y: newValue.y,
+      type: newValue.type,
+    };
+    setCount(count + 1);
+    setArr([...arr, content]);
+  };
+
+  function max(list) {
+    if (list.y + list.height > maxHeight + Height) {
+      setmaxHeight(list.y);
+      setHeight(list.height);
+    }
+  }
+
+  const getData = (data) => {
+    setArr(
+      arr.map((list) => (list.id === data.id ? { ...list, ...data } : list))
+    );
+  };
+
+  function lists(list) {
+    switch (list.type) {
+      case 1:
+        return (
+          <TextArea key={list.id} list={list} propsFunction={getData}>
+            {max(list)}
+          </TextArea>
+        );
+      //   case 2:
+      //     return <ImageListAccordion key={list.id} data={list} />;
+      //   case 4:
+      //     return <HyperLink key={list.id} list={list} />;
+      //   default:
+      //     return (
+      //       <Card
+      //       key={list.id}
+      //       sx={{
+      //         position: 'absolute',
+      //         width: list.width,
+      //         height: list.height,
+      //         borderRadius: 2,
+      //         textAlign: 'center',
+      //         fontSize: '0.875rem',
+      //         fontWeight: '700',
+      //         top: list.coordinateY,
+      //         left: list.coordinateX,
+      //         type: list.type,
+      //         border: 1,
+      //       }}
+      //     >
+      //       <CardContent>
+      //         {max(list)}
+      //         {list.type}
+      //       </CardContent>
+      //     </Card>
+      //     );
+    }
+  }
+
+  const layoutlist = arr.map((list) => lists(list));
 
   return (
     <Box
@@ -54,23 +129,34 @@ export default function WriteContent() {
           <Tab
             icon={<ArticleOutlinedIcon />}
             aria-label="ArticleOutlinedIcon"
-            value='content'
+            value="content"
+            onClick={() =>
+              handleArrChange({
+                id: count,
+                context: '',
+                width: 200,
+                height: 200,
+                x: 0,
+                y: 0,
+                type: 1,
+              })
+            }
           />
-          <Tab label="Item Two" value='2'/>
-          <Tab label="Item Three" value='3'/>
-          <Tab label="Item One" value='4'/>
-          <Tab label="Item Two" value='5'/>
-          <Tab icon={<Dropdown />} aria-label="phone" value='6'/>
+          <Tab label="Item Two" value="2" />
+          <Tab label="Item Three" value="3" />
+          <Tab label="Item One" value="4" />
+          <Tab label="Item Two" value="5" />
+          <Tab icon={<Dropdown />} aria-label="phone" value="6" />
         </Tabs>
       </Box>
       <Box
         sx={{
+          position: 'relative',
           width: '90%',
-          marginLeft: '5%',
-          height: 800,
+          height: 30 + maxHeight + Height,
         }}
       >
-        
+        {layoutlist}
       </Box>
     </Box>
   );
