dd6defeec1fe92fd9aac2d541a3de7ae03ae2fab
been232 <kean503@naver.com>
2022-07-13 19:10:10 +0900

    resloved conflict

--- front/src/UI/templates/App.js
+++ front/src/UI/templates/App.js
@@ -5,30 +5,22 @@ import BoardDetailPage from '../pages/BoardPage/BoardDetailPage';
 import MainPage from '../pages/MainPage/MainPage';
 import LayoutPage from '../pages/LayoutPage/LayoutPage';
 import ControlledOpenSpeedDial from '../atoms/MainPage/ControlledOpenSpeedDial';
-<<<<<<< HEAD
 import LoginPopupPage from '../pages/LoginPopupPage';
 import SignUpPage from '../pages/SignUpPage';
-=======
-import BoardWritePage from '../pages/BoardPage/BoardWritePage';
->>>>>>> 0323e6b5701b8a689eb4022dadf33d5fffc6488a
 
 const App = () => {
   return (
     <>
       <div className="App">
         <BrowserRouter>
-          <HeaderOrganisms sx={{marginBottom : 10}}/>
+          <HeaderOrganisms sx={{ marginBottom: 10 }} />
           <Routes>
             <Route path="/" element={<MainPage />} />
-            <Route path="/Layout/*" element={<LayoutPage />}/>
-            <Route path="/BoardDetail/*" element={<BoardDetailPage />}/>
-<<<<<<< HEAD
-            <Route path="/sign/*" element={<SignUpPage />}/>
-            <Route path="/login/*" element={<LoginPopupPage />}/>
-            <Route path="/findID/*" element={<LoginPopupPage />}/>
-=======
-            <Route path="/BoardWrite/*" element={<BoardWritePage />}/>
->>>>>>> 0323e6b5701b8a689eb4022dadf33d5fffc6488a
+            <Route path="/Layout/*" element={<LayoutPage />} />
+            <Route path="/BoardDetail/*" element={<BoardDetailPage />} />
+            <Route path="/sign/*" element={<SignUpPage />} />
+            <Route path="/login/*" element={<LoginPopupPage />} />
+            <Route path="/findID/*" element={<LoginPopupPage />} />
           </Routes>
           <ControlledOpenSpeedDial />
         </BrowserRouter>
