2fed770157dac6012ca6ec0fd9dacf7356d909a6
ilovedeo <55009603+ilovedeo@users.noreply.github.com>
2022-08-04 16:18:52 +0900

    통계 년도 전달

--- front/src/UI/organisms/StatisticsPage/StatisticsContent.js
+++ front/src/UI/organisms/StatisticsPage/StatisticsContent.js
@@ -1,23 +1,37 @@
 import Box from '@mui/material/Box';
 import { useEffect, useRef, useState, useCallback } from 'react';
 import StatisticsCount from '../../molecules/StatisticsPage/StatisticsCount';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
 
 export default function StatisticsContent(props) {
     const data = props.data[0].data[0];
+    const [text, setText] = useState('');
+
+    const handleChange = (event) => {
+      setText(event.target.value);
+    };
+
+    const handle = () => {
+      props.propFunction(text);
+    }
+    
+    useEffect(() => {}, [text])
 
   return (
       <Box
         sx={{
           width: '90%',
-          height: 100,
+          height: 150,
           marginLeft: '5%',
           border: 2,
-          marginTop: 5,
           display: 'inline-block'
         }}
       >
-        <StatisticsCount data={data}/>
+        <StatisticsCount data={data} />
         <h2 style={{marginLeft: '5%'}}>내 통계</h2>
+        <Button onClick={handle} style={{display: 'inline-block', float: 'right', marginTop: 15}}>확인</Button>
+        <TextField type="number" style={{display: 'inline-block', float: 'right'}} id="standard-basic" onChange={handleChange} label="년도 입력(숫자만)" variant="standard" />
     </Box>
   );
 }
