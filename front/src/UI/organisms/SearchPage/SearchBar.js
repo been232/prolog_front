dc3e35eaf5d1014ed9f3ab46f5e234167366b50c
sojeongEom <esj5029@gmail.com>
2022-08-04 14:50:40 +0900

    (sojeong) merge

--- front/src/UI/organisms/SearchPage/SearchBar.js
+++ front/src/UI/organisms/SearchPage/SearchBar.js
@@ -1,12 +1,7 @@
 import { Box } from '@mui/material';
 import React from 'react';
-<<<<<<< HEAD
-// import { SearchTextField } from '../../atoms/Commons/TextField';
-import SearchButton from '../../atoms/SearchPage/SearchButton';
-=======
 import { SearchTextField } from '../../atoms/Commons/TextField';
 import { SearchButton } from '../../atoms/SearchPage/SearchButton';
->>>>>>> 94677d9971af7e87c81492ae1a9d04353ea7955e
 
 function SearchBar() {
   const getSearch = () => {
@@ -26,13 +21,8 @@ function SearchBar() {
     >
       {/* <SearchTextField
         style={{ width: '500px', marginRight: '10px' }}
-<<<<<<< HEAD
       ></SearchTextField> */}
       <SearchButton></SearchButton>
-=======
-      ></SearchTextField>
-      <SearchButton onClick={getSearch}></SearchButton>
->>>>>>> 94677d9971af7e87c81492ae1a9d04353ea7955e
     </Box>
   );
 }
