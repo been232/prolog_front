01f729d8d822d3f32c5a9daa28206a1991d84290
been232 <kean503@naver.com>
2022-08-07 21:36:52 +0900

    .

--- front/src/UI/organisms/SearchPage/SearchBar.js
+++ front/src/UI/organisms/SearchPage/SearchBar.js
@@ -3,10 +3,45 @@ import React from 'react';
 import { SearchTextField } from '../../atoms/Commons/TextField';
 import { SearchButton } from '../../atoms/SearchPage/SearchButton';
 
-function SearchBar() {
+function SearchBar(props) {
+  const setDatas = props.setDatas;
   const getSearch = () => {
     //검색하는 통신
     console.log('test');
+    setDatas({
+      success: true,
+      data: [
+        {
+          id: 'postId',
+          title: 'title',
+          written: '2022-06-07',
+          member: 'memberName',
+          memberImage: 'http://~',
+          likes: '8',
+          mainLayout: {
+            type: '1',
+            width: '12',
+            height: '35',
+            content: '어쩌구 저쩌구',
+          },
+        },
+        {
+          id: 'postId',
+          title: 'title',
+          content: '어쩌구 저쩌구',
+          written: '2022-06-07',
+          member: 'memberName',
+          memberImage: 'http://~',
+          likes: '8',
+          mainLayout: {
+            type: '1',
+            width: '12',
+            height: '35',
+            content: '어쩌구 저쩌구',
+          },
+        },
+      ],
+    });
   };
   return (
     <Box
@@ -19,10 +54,10 @@ function SearchBar() {
         alignItems: 'center',
       }}
     >
-      {/* <SearchTextField
+      <SearchTextField
         style={{ width: '500px', marginRight: '10px' }}
-      ></SearchTextField> */}
-      <SearchButton></SearchButton>
+      ></SearchTextField>
+      <SearchButton onClick={getSearch}></SearchButton>
     </Box>
   );
 }
--- front/src/UI/pages/SearchPage/SearchPage.js
+++ front/src/UI/pages/SearchPage/SearchPage.js
@@ -1,46 +1,47 @@
 import { Box, Grid } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import TextBar from '../../atoms/SearchPage/TextBar';
 import MainPageOrganism from '../../organisms/MainPage/MainPageOrganism';
 import SearchBar from '../../organisms/SearchPage/SearchBar';
 function SearchPage() {
-  const datas = {
-    success: true,
-    data: [
-      {
-        id: 'postId',
-        title: 'title',
-        written: '2022-06-07',
-        member: 'memberName',
-        memberImage: 'http://~',
-        likes: '8',
-        mainLayout: {
-          type: '1',
-          width: '12',
-          height: '35',
-          content: '어쩌구 저쩌구',
-        },
-      },
-      {
-        id: 'postId',
-        title: 'title',
-        content: '어쩌구 저쩌구',
-        written: '2022-06-07',
-        member: 'memberName',
-        memberImage: 'http://~',
-        likes: '8',
-        mainLayout: {
-          type: '1',
-          width: '12',
-          height: '35',
-          content: '어쩌구 저쩌구',
-        },
-      },
-    ],
-  };
+  const [datas, setDatas] = useState();
+  // const datas = {
+  //   success: true,
+  //   data: [
+  //     {
+  //       id: 'postId',
+  //       title: 'title',
+  //       written: '2022-06-07',
+  //       member: 'memberName',
+  //       memberImage: 'http://~',
+  //       likes: '8',
+  //       mainLayout: {
+  //         type: '1',
+  //         width: '12',
+  //         height: '35',
+  //         content: '어쩌구 저쩌구',
+  //       },
+  //     },
+  //     {
+  //       id: 'postId',
+  //       title: 'title',
+  //       content: '어쩌구 저쩌구',
+  //       written: '2022-06-07',
+  //       member: 'memberName',
+  //       memberImage: 'http://~',
+  //       likes: '8',
+  //       mainLayout: {
+  //         type: '1',
+  //         width: '12',
+  //         height: '35',
+  //         content: '어쩌구 저쩌구',
+  //       },
+  //     },
+  //   ],
+  // };
   return (
     <Box sx={{ marginLeft: '10%', marginRight: '10%' }}>
-      <SearchBar></SearchBar>
+      <SearchBar setDatas={setDatas}></SearchBar>
       <Grid container spacing={3} sx={{ marginTop: '3%' }}>
         {datas &&
           datas.data.map((data) => (
