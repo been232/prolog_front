1b36f2ec70405c891cb058540ec6c7cacb718580
sojeongEom <esj5029@gmail.com>
2022-08-10 14:07:22 +0900

    (sojeong) 수정 작업

--- front/src/UI/atoms/MyPage/MyBoardAtoms.js
+++ front/src/UI/atoms/MyPage/MyBoardAtoms.js
@@ -1,9 +1,11 @@
-import { Box, Button, IconButton, Modal } from '@mui/material';
+import { Box, Button, IconButton, Modal, TextField } from '@mui/material';
 import React, { useEffect, useRef, useState } from 'react';
 import PostBoxBottomMolecule from '../../molecules/MainPage/PostBoxBottomMolecule';
 import PostBoxMiddleMolecule from '../../molecules/MainPage/PostBoxMiddleMolecule';
 import PostBoxTopMolecule from '../../molecules/MainPage/PostBoxTopMolecule';
 import ChartList from '../../organisms/StatisticsPage/ChartList';
+import UnderButton from '../../molecules/StatisticsPage/UnderButton';
+import LinkButton from '../../atoms/MyPage/LinkButton';
 import CloseIcon from '@mui/icons-material/Close';
 // import { useInterval } from './useInterval';
 const style = {
@@ -45,19 +47,45 @@ const data = [
 ];
 
 function MyBoardAtoms(props) {
+  const { id, title, written, member, memberImage, likes, mainLayout } = props.data;
   const [open, setOpen] = useState(false);
   var timer;
+  const [text, setText] = useState(undefined);
+  const [visible, setVisible] = useState(false);
+
   const handleOpen = () => {
     timer = setTimeout(() => {
       setOpen(true);
     }, 1000);
   };
+
   const handleClose = () => {
     setOpen(false);
     clearTimeout(timer);
   };
-  const { id, title, written, member, memberImage, likes, mainLayout } =
-    props.data;
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleCheck = (event) => {
+    // id와 text값을 전달해서 통신하는 코드 작성하기
+    console.log("통계 불러오기 완료");
+
+    const response = {
+      "success": true,
+    }
+
+    if (response.success) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+
+  };
+
+  useEffect(() => { }, [text])
+
   return (
     <Box
       sx={{
@@ -67,17 +95,56 @@ function MyBoardAtoms(props) {
       }}
       onMouseEnter={handleOpen}
       onMouseLeave={handleClose}
+      style={{ fontFamily: 'SUIT-Medium' }}
     >
       <Modal open={open} onClose={handleClose}>
-        <Box sx={{ ...style, width: 400, height: 600 }}>
-          <IconButton
-            sx={{ position: 'fixed', top: 0, right: 0 }}
-            onClick={handleClose}
-          >
-            <CloseIcon />
-          </IconButton>
-          <ChartList data={data} />
-        </Box>
+        {
+          (visible) ? (
+            <Box sx={{ ...style, width: 400, height: 600 }}>
+              <IconButton
+                sx={{ position: 'fixed', top: 0, right: 0 }}
+                onClick={handleClose}
+              >
+                <CloseIcon />
+              </IconButton>
+              <Box
+                sx={{
+                  width: '90%',
+                  height: 100,
+                  marginLeft: '5%',
+                  display: 'inline-block'
+                }}>
+                <Button onClick={handleCheck} style={{ display: 'inline-block', float: 'right', marginTop: 15 }}>확인</Button>
+                <TextField type="number" style={{ display: 'inline-block', float: 'right' }} id="standard-basic" onChange={handleChange} label="년도 입력(숫자만)" variant="standard" />
+              </Box>
+              <ChartList data={data} text={text} />
+              <UnderButton />
+              <LinkButton id={id}/>
+            </Box>
+
+          ) : (
+            <Box sx={{ ...style, width: 400, height: 600 }}>
+              <IconButton
+                sx={{ position: 'fixed', top: 0, right: 0 }}
+                onClick={handleClose}
+              >
+                <CloseIcon />
+              </IconButton>
+              <Box
+                sx={{
+                  width: '90%',
+                  height: 100,
+                  marginLeft: '5%',
+                  display: 'inline-block'
+                }}>
+                <Button onClick={handleCheck} style={{ display: 'inline-block', float: 'right', marginTop: 15 }}>확인</Button>
+                <TextField type="number" style={{ display: 'inline-block', float: 'right' }} id="standard-basic" onChange={handleChange} label="년도 입력(숫자만)" variant="standard" />
+              </Box>
+              <UnderButton />
+              <LinkButton id={id}/>
+            </Box>
+          )
+        }
       </Modal>
       <PostBoxTopMolecule />
       <PostBoxMiddleMolecule title={title} />
