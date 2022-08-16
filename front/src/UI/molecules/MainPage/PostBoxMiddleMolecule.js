e691d76be0ce6aec52b4f14a018333549ff15a81
been232 <kean503@naver.com>
2022-08-16 23:02:31 +0900

    게시글 링크

--- front/src/UI/molecules/MainPage/PostBoxMiddleMolecule.js
+++ front/src/UI/molecules/MainPage/PostBoxMiddleMolecule.js
@@ -1,11 +1,14 @@
 import { Box } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function PostBoxMiddleMolecule({ title }) {
   return (
     /* padding min-height 재설정 필요함. */
     <Box sx={{ minHeight: 125, padding: 1.5 }}>
-      <p>{title}</p>
+      <Link to="/BoardDetail">
+        <p>{title}</p>
+      </Link>
     </Box>
   );
 }
--- front/src/UI/molecules/MainPage/PostBoxTopMolecule.js
+++ front/src/UI/molecules/MainPage/PostBoxTopMolecule.js
@@ -1,12 +1,15 @@
 import { Box } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function PostBoxTopMolecule() {
   return (
     /* 가시성 때문에 padding 설정함.*/
     <Box sx={{ padding: 1.5 }}>
-      <img />
-      <p>이미지 들어갈 공간임.</p>
+      <Link to="/BoardDetail">
+        <img />
+        <p>이미지 들어갈 공간임.</p>
+      </Link>
     </Box>
   );
 }
