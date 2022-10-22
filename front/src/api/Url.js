fc5335ef5e721687420279cff242343deba717b5
sojeongEom <esj5029@gmail.com>
2022-10-22 22:12:27 +0900

    (sojeong) url 오류 수정

--- front/src/api/Url.js
+++ front/src/api/Url.js
@@ -1,9 +1,5 @@
 // export const base_url = 'http://113.59.178.4:9000';
-<<<<<<< HEAD
-// export const base_url = 'https://9a4c-202-31-202-184.jp.ngrok.io';
-=======
 // export const base_url = 'https://3ad4-112-217-167-202.jp.ngrok.io';
->>>>>>> 2388f9f86a8d1a705620e8aa85d91d6caed9b341
 // export const base_url = 'https://192.168.235.176:8080';
 // export const base_url = '';
 export const base_url = 'http://210.91.148.88:8080';
